e607a39cf42d08f18487e681df2516b0ed1f9f68
Andreas Svanberg <andreas.svanberg@mensa.se>
2016-05-09 21:09:18 +0200

    Set a good title for what the page shows.

--- client/js/app.js
+++ client/js/app.js
@@ -31,6 +31,7 @@
           allianceColors: this.allianceColor()
         });
         document.location.hash = hash;
+        document.title = `Fight in ${this.solarSystems().map(s => s.name).join(', ')} on ${this.from().toDateString()}`
 
         z.fetchAll(this.solarSystems(), this.from(), this.to())
           .then(this.kms)
